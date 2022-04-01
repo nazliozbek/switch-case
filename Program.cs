77381a9dfdcdc8b7c69495c9225d7375acda459f
nazliozbek <102694546+nazliozbek@users.noreply.github.com>
2022-04-01 23:20:57 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace switch_case
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int month = DateTime.Now.Month;
+            switch(month)
+            {
+                case 1:
+                    Console.WriteLine("ocak ayi");
+                    break;
+                case 2:
+                    Console.WriteLine("subat ayi");
+                    break;
+                case 3:
+                    Console.WriteLine("mart ayi");
+                    break;
+                case 4:
+                    Console.WriteLine("nisan ayi");
+                    break;
+                default:
+                    Console.WriteLine("yanlis veri");
+                    break;
+            }
+
+            switch(month)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    Console.WriteLine("kis mevsimi");
+                    break;
+
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("ilkbahar");
+                    break;
+
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine(" yaz mevsimi");
+                    break;
+
+                case 9:
+                case 10:
+                case 11:
+                    Console.WriteLine("sonbahar");
+                    break;
+            }
+        }
+    }
+}
